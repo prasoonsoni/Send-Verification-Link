d71dd7e3ebe4feb90e8cc44c42707116a36c1b8b
Prasoon <prasoonsoni.work@gmail.com>
2022-06-07 11:26:39 +0530

    Initial Commit 🚀

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = 3000
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('<center><h1>Welcome to Mail Verification</h1>' +
+        '<h3><a href="https://github.com/prasoonsoni/Mail-Verification" target="_blank">Visit Repository</a></h3></center>')
+})
+
+app.listen(port, () => {
+    console.log(`Mail Verification listening on http://localhost:${port}`)
+})
