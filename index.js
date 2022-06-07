985fabf2cc30e778c29edc96a92befb4d36443e1
Prasoon <prasoonsoni.work@gmail.com>
2022-06-07 11:42:59 +0530

    Database Connection Done 🔗

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = 3000
+const connectToDatabase = require('./database/connection')
+connectToDatabase()
 
 app.use(cors())
 app.use(express.json())
